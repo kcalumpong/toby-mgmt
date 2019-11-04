14d97838cd9d50d917277e7af4e7de67acadb77b
Kristina Calumpong <kcalumpong@gmail.com>
2019-11-03 16:56:38 -0800

    added onChange

--- src/components/Job/index.js
+++ src/components/Job/index.js
@@ -1,24 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 
-const Job = () => {
-    return (
-        <div className="job-page">
-            <form name="job-form">
+class Job extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hireDate: "",
+            status: "",
+            employmentDate: "",
+            department: "",
+            title: "",
+            location: "",
+            reportsTo: "",
+            compensationDate: "",
+            payRate: 0,
+            payType: "",
+            paySchedule: "",
+            changeReason: "",
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        let name = target.name;
+        // const date = target.date,
+        let value = target.value;
+
+        this.setState({
+
+            [name]: value
+
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.name);
+        console.log(this.state.value);
+    }
+
+    render() {
+        return (
+            <form name="job-form" onSubmit={this.handleSubmit}>
                 <button className="job-form-button" form name="job-form">SAVE</button>
 
                 <div className="hire-and-status">
                     <div className="hire-input">
                         <h4>Employment</h4>
                         <h3>Hire Date</h3>
-                        <input type="date" name="hire-date"></input>
+                        <input type="date" name="hire-date" value={this.state.hireDate} onChange={this.handleInputChange}></input>
                     </div>
 
                     <div className="active-input">
                         <h3>Active</h3>
-                        <select>
+                        <select value={this.state.value} onChange={this.handleInputChange}>
                             <option value="active" name="status">Active</option>
-                            <option value="not-active" name="status">Not Active</option>
+                            <option value="not-active" name="status" >Not Active</option>
                         </select>
                     </div>
                 </div>
@@ -28,27 +67,27 @@ const Job = () => {
                     <div className="effective-date-input">
                         <h4>Job Information</h4>
                         <h3>Effective Date</h3>
-                        <input type="date" name="effective-date"></input>
+                        <input type="date" name="hireDate" value={this.state.employmentDate} onChange={this.handleInputChange}></input>
                     </div>
 
                     <div className="dept-input">
                         <h3>Dept</h3>
-                        <input type="text" name="dept" placeholder="Marketing"></input>
+                        <input type="text" name="department" value={this.state.department} onChange={this.handleInputChange}></input>
                     </div>
 
                     <div className="title-input">
                         <h3>Title</h3>
-                        <input type="text" name="title" placeholder="Marketing Director"></input>
+                        <input type="text" name="title" value={this.state.title} onChange={this.handleInputChange}></input>
                     </div>
 
                     <div className="location-input">
                         <h3>Location</h3>
-                        <input type="text" name="location" placeholder="Location"></input>
+                        <input type="text" name="location" value={this.state.location} onChange={this.handleInputChange}></input>
                     </div>
 
                     <div className="reports-to-input">
                         <h3>Reports to</h3>
-                        <input type="text" name="location"></input>
+                        <input type="text" name="reportsTo" value={this.state.reportsTo} onChange={this.handleInputChange}></input>
                     </div>
 
                 </div>
@@ -58,36 +97,34 @@ const Job = () => {
                     <div className="effective-date-input-two">
                         <h4>Compensation</h4>
                         <h3>Effective Date</h3>
-                        <input type="date" name="effective-date"></input>
+                        <input type="date" name="compensationDate" value={this.state.compensationDate} onChange={this.handleInputChange}></input>
                     </div>
 
                     <div className="pay-rate">
                         <h3>Pay Rate</h3>
-                        <input type="text" name="payrate"></input>
+                        <input type="text" name="payRate" value={this.state.payRate} onChange={this.handleInputChange}></input>
                     </div>
 
                     <div className="pay-type">
                         <h3>Pay Type</h3>
-                        <input type="text" name="paytype" placeholder="Salary"></input>
+                        <input type="text" name="payType" value={this.state.payType} onChange={this.handleInputChange}></input>
                     </div>
 
                     <div className="pay-schedule">
                         <h3>Pay Schedule</h3>
-                        <input type="text" name="paytype" placeholder="Every Two Weeks"></input>
-                     </div>
-
-                        <div className="change-reason">
-                            <h3>Change Reason</h3>
-                            <input type="text" name="paytype" placeholder="Promotion"></input>
-                        </div>
-
+                        <input type="text" name="paySchedule" value={this.state.paySchedule} onChange={this.handleInputChange}></input>
                     </div>
-    
+
+                    <div className="change-reason">
+                        <h3>Change Reason</h3>
+                        <input type="text" name="changeReason" value={this.state.changeReason} onChange={this.handleInputChange}></input>
+                    </div>
+
+                </div>
+
             </form>
-
-        </div>
-
-    )
+        )
+    }
 }
 
 export default Job;
