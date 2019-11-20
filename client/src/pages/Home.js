b403cf7bde9b897ac4841d895b243a9ae40ffcf7
Noel Cruz <noel.j.cruz@outlook.com>
2019-11-19 21:47:07 -0800

    Combined branches and resolved code conflicts.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,22 +2,38 @@ import React, { Component} from "react";
 import { Redirect } from 'react-router-dom';
 import Navtabs from "../components/Navtabs";
 import API from "../utils/API";
-// import Profile from "../components/Profile";
+import Profile from "../components/Profile";
 import "../App.css";
 
 
 class Home extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            loggedIn: false,
-            loading: true
-        };
+    state = {
+        loggedIn: false,
+        loading: true,
+        employees: [
+            // {
+            //     id: 1,
+            //     name: "",
+            //     title: "",
+            //     img: ""
+            // }
+        ]
     }
 
     componentDidMount() {
         this.checkAuth();
+        fetch("https://dog.ceo/api/breeds/image/random/5")
+            .then(res => res.json())
+            .then(data => data.message.map(item => (
+                {
+                    id: 1,
+                    name: "dog",
+                    title: "good boy",
+                    img: item
+                }
+            )))
+            .then(data => this.setState({ employees: data }))
+            .catch(err => console.err(err))
     }
 
     checkAuth() {
@@ -63,6 +79,14 @@ class Home extends Component {
             <h4>Employee List</h4>
             <h5>Select any employee to begin</h5>
             <div className="individuals-container">
+                {this.state.employees.map(item => (
+                    <Profile
+                        id={item.id}
+                        name={item.name}
+                        title={item.title}
+                        img={item.img}
+                    />
+                ))}
                 {/* {this.state.employee.map(item => (
                     <Profile
                         key={`individuals-${item}`}
